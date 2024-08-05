2280dc40f8346737889c77aa08fc591131c527a8
abdeldjalil dahmani <91489602+jeo17@users.noreply.github.com>
2024-08-05 16:04:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/account': {
+        target: 'https://auto-reporting-server.sphinx-international.online',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/account/, '/account')
+      },
+      '/workorder': {
+        target: 'https://auto-reporting-server.sphinx-international.online',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/workorder/, '/workorder')
+      },
+      '/ws': {
+        target: 'wss://auto-reporting-server.sphinx-international.online',
+        changeOrigin: true,
+        ws: true, // Enable WebSocket proxying
+        rewrite: (path) => path.replace(/^\/ws/, '/ws')
+      },
+    },
+  },
   plugins: [react()],
 });
